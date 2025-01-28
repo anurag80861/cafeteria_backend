51e39be91061c36422439cd39674385a9c3cc3ee
anurag80861 <anurag95571@gmail.com>
2025-01-29 00:52:48 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+const Dish = require('./dish');
 
 const CartItemSchema = new mongoose.Schema({
     dish: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Dish",
         required: true,
-        Validate: {
-            Validator: async function (dishId) {
-                const dishExists = await mongoose.model('Dish').exists({ _id: dishId })
-                return dishExists
+        validate: {
+            validator: async function (dishId) {
+                return await Dish.exists({ _id: dishId, inStock: true });
             },
             message: 'Invalid Dish reference'
         }
@@ -18,15 +18,15 @@ const CartItemSchema = new mongoose.Schema({
         required: true,
         min: [1, 'Quantity must be at least 1']
     },
-})
+});
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     role: { type: String, enum: ['Customer', 'Merchant', 'Admin'], required: true },
     cart: { type: [CartItemSchema] }
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
-module.exports=User
+module.exports = User;
